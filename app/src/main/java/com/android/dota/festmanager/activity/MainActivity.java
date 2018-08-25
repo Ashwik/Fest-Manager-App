7627b2f2e4d76fcb2e424cf7cb3173fd4b9a820f
Kartheek Akella <sukruthkartheek@gmail.com>
2018-08-25 22:38:30 +0530

    Complete adding app drawer

--- app/src/main/java/com/android/dota/festmanager/activity/MainActivity.java
+++ app/src/main/java/com/android/dota/festmanager/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.android.dota.festmanager.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.android.dota.festmanager.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      return true;
     }
 }
