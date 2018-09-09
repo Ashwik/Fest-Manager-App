03d1932bd48b1e74ee53f1191d434522d4c9c923
leDarkKnight <vamsis.adapa@gmail.com>
2018-09-09 12:44:38 +0530

    commited about contact and reachus

--- app/src/main/java/com/android/dota/festmanager/fragment/ReachUs.java
+++ app/src/main/java/com/android/dota/festmanager/fragment/ReachUs.java
@@ -1,17 +1,19 @@
 package com.android.dota.festmanager.fragment;
 
-
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
 import com.android.dota.festmanager.R;
 
-public class AboutFragment extends Fragment {
+
+public class ReachUs extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,11 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about,container,false);
+        return inflater.inflate(R.layout.fragment_reach_us,container,false);
     }
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        TextView about = getView().findViewById(R.id.textView);
-        about.setText(getString(R.string.aboutus));
+        TextView about = getView().findViewById(R.id.reachus);
+        about.setText(getString(R.string.directions));
     }
 }
