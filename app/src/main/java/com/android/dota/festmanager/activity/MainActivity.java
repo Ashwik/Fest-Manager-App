aa7cd514499c80de040055ac1b29653dadb7f02e
ajithkanduri <37373929+ajithkanduri@users.noreply.github.com>
2018-09-09 14:00:53 +0530

    commited about contact and reachus (#7)
    TODO: Clean styles and colors

--- app/src/main/java/com/android/dota/festmanager/activity/MainActivity.java
+++ app/src/main/java/com/android/dota/festmanager/activity/MainActivity.java
@@ -1,19 +1,16 @@
 package com.android.dota.festmanager.activity;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.android.dota.festmanager.R;
@@ -22,6 +19,7 @@ import com.android.dota.festmanager.fragment.ContactsFragment;
 import com.android.dota.festmanager.fragment.CreditsFragment;
 import com.android.dota.festmanager.fragment.GuideFragment;
 import com.android.dota.festmanager.fragment.HomeFragment;
+import com.android.dota.festmanager.fragment.ReachUs;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -98,6 +96,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.credits:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.nav_fragment_container,new CreditsFragment())
+                        .commit();
+                break;
+            case R.id.reach:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.nav_fragment_container,new ReachUs())
                         .commit();
                 break;
         }
