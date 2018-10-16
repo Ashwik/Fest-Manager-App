eea5ba5408b668f737a9cc5bfe2cf0beee27d070
Ashwik <ashwikreddyaileni999@gmail.com>
2018-10-16 14:06:22 +0530

    EventFragment Ui changes

---
+++ app/src/main/java/com/dota/festmanager/fragment/EventCardsFragment.java
@@ -0,0 +1,59 @@
+package com.dota.festmanager.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.azoft.carousellayoutmanager.CarouselLayoutManager;
+import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
+import com.dota.festmanager.R;
+import com.dota.festmanager.adapter.ScheduleCardAdapter;
+
+import java.util.ArrayList;
+
+public class EventCardsFragment extends Fragment {
+
+    private Context context;
+    private RecyclerView recyclerView;
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_schedule_cards,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle("Schedule");
+
+        final CarouselLayoutManager layoutManager = new CarouselLayoutManager(CarouselLayoutManager.VERTICAL, false);
+        layoutManager.setPostLayoutListener(new CarouselZoomPostLayoutListener());
+
+        recyclerView = getActivity().findViewById(R.id.schedule_card_recycler_view);
+        list.add(R.drawable.competitions);
+        list.add(R.drawable.competitions);
+        list.add(R.drawable.competitions);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new ScheduleCardAdapter(list,context));;
+    }
+}
