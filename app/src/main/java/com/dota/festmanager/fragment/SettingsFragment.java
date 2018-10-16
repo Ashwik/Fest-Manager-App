59d2746088d5422577f8b1044c2cfe96edc136fe
620tx <raghavmessi32@gmail.com>
2018-10-16 16:14:00 +0530

    Changed Settings

--- app/src/main/java/com/dota/festmanager/fragment/SettingsFragment.java
+++ app/src/main/java/com/dota/festmanager/fragment/SettingsFragment.java
@@ -29,7 +29,7 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
         @Override
         public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, Bundle
         savedInstanceState){
-            return inflater.inflate(R.layout.fragment_maps, container, false);
+            return inflater.inflate(R.layout.fragment_settings, container, false);
         }
         public void onViewCreated (View view, @Nullable Bundle savedInstanceState)
         {
