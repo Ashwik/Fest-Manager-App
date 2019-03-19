1297cde4f6b232d8231d814170a096ed079fe3ef
Ashwik <ashwikreddyaileni999@gmail.com>
2019-03-19 10:18:34 +0530

    Fixes Contact us UI

--- app/src/main/java/com/dota/pearl2019/fragment/ContactsFragment.java
+++ app/src/main/java/com/dota/pearl2019/fragment/ContactsFragment.java
@@ -1,9 +1,11 @@
 package com.dota.pearl2019.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
@@ -17,6 +19,7 @@ import com.dota.pearl2019.R;
 public class ContactsFragment extends Fragment {
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
+    private FragmentActivity mycontext;
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,5 +60,23 @@ public class ContactsFragment extends Fragment {
         public int getCount() {
             return 2;
         }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Organising Body";
+                case 1:
+                    return "Club Senate";
+            }
+            return null;
+        }
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        mycontext = (FragmentActivity) activity;
+        super.onAttach(activity);
     }
 }
